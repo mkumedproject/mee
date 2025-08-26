efe631085d7d787e70a13d3a14076b4936815710
hydrocephcare <hydrocephcare@gmail.com>
2025-08-26 12:33:30 +0300

    Updated LecturersManager.tsx

---
+++ src/pages/admin/LecturersManager.tsx
@@ -0,0 +1,14 @@
+// NotesManager.tsx
+export default function NotesManager() {
+  return <h1>Notes Manager</h1>;
+}
+
+// UnitsManager.tsx
+export default function UnitsManager() {
+  return <h1>Units Manager</h1>;
+}
+
+// LecturersManager.tsx
+export default function LecturersManager() {
+  return <h1>Lecturers Manager</h1>;
+}
